5f14cd0bbcd3860575d941cd7acb31648dc20842
kenjikoba <kenji.kobayashi0419@gmail.com>
2022-06-12 22:36:07 +0900

    Layout追加

---
+++ components/footer.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import styles from '../styles/footer.module.css'
+
+export default function Footer() {
+    return (
+        <div className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          OverPowered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </div>
+    )
+}

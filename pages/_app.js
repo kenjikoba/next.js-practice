4f152818f089b64e5ede7427922047bcad755813
kenjikoba <kenji.kobayashi0419@gmail.com>
2022-06-15 12:04:07 +0900

    googlemap追加

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,22 @@ import 'tailwindcss/tailwind.css'
 import Layout from '../components/layout'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+{/** やりたいこと
 
+1. Google Mapのピン立て（東大のところでいいかな？）
+2. Contactフォームを作る
+3. 
+
+
+
+
+
+
+
+
+
+
+*/}
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/profile.js
+++ pages/profile.js
@@ -2,8 +2,28 @@ import Card from '../components/card'
 import styles from '../styles/Home.module.css'
 import { useEffect,useState } from 'react'
 import Head from 'next/head'
+import GoogleMapReact from 'google-map-react'
+
 
 export default function Profile() {
+  // 東大の緯度・経度を設定
+  const defaultLatLng = {
+    lat: 35.712677,
+    lng: 139.761989
+  };
+  // マーカーを設定
+  const handleApiLoaded = ({ map, maps }) => {
+    new maps.Marker({
+      map,
+      position: defaultLatLng,
+    });
+  };
+  // クリック設定
+  const setLatLng = ({ x, y, lat, lng, event }) => {
+    console.log(lat);
+    console.log(lng);
+  };
+  
   const [users, setUsers] = useState([])
   useEffect(() => {
     const fetchUsers = async () => {
@@ -19,6 +39,16 @@ export default function Profile() {
         <Head>
         <title>Profile</title>
       </Head>
+
+      <div style={{ height: '600px', width: '800px' }}>
+        <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_OW_MAPS_API_KEY }}
+        defaultCenter={defaultLatLng}
+        defaultZoom={16}
+        onGoogleApiLoaded={handleApiLoaded}
+        onClick={setLatLng}
+        />
+      </div>
  
       <ul>
         {users.map(user => (
